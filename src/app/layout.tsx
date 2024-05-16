6851e39ee84758496edd3ebc694afd47507e1dbc
zei <intoavortex@gmail.com>
2024-05-16 22:13:42 +0900

    layout, component split setting(not yet)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/layout/SideBar";
+import Header from "@/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <SideBar />
+          <div>
+            <Header />
+            {children}
+          </div>
+        </div>
+
+      </body>
     </html>
   );
 }
